8fa8679f65be3ea8d3c8ed1dbb7ee8cde463c481
ptn1411 <57529765+ptn1411@users.noreply.github.com>
2021-09-12 21:37:27 +0700

    form admin

---
+++ WindowsFormsApp1/WindowsFormsApp1/AccountProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class AccountProfile : Form
+    {
+        public AccountProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+           this.Close();
+           
+        }
+    }
+}
